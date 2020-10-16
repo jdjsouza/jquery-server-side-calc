b02a41c75005d16d7952c802b06f05c7abd92fa6
xarsin <jdj.souza@gmail.com>
2020-10-16 17:53:40 -0500

    created mathClear function for C button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,22 @@
 $(document).ready(onReady);
+let mathOp;
 
 function onReady() {
   console.log('JQ Loaded');
 
   $('.js-btn-math').on('click', mathOperator);
+  $('.js-btn-clear').on('click', mathClear);
 }
 
 function mathOperator() {
   $(this).parent().find('button').removeClass('button-clicked');
   $(this).addClass('button-clicked');
-  let mathOp = $(this).prop('name');
-  console.log('Math Operator', mathOp);
+  mathOp = $(this).prop('name');
+}
+
+function mathClear() {
+  $(this).parent().find('button').removeClass('button-clicked');
+  $('.js-input-first').val('');
+  $('.js-input-second').val('');
+  mathOp = null;
 }
