8c97a660b58aa1d14c3d2c664a365ebced9c0da2
xarsin <jdj.souza@gmail.com>
2020-10-16 20:49:01 -0500

    POST is working, math happens correctly and stores the history, added GET functions, added render functions, need to configure renders

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,114 @@
 $(document).ready(onReady);
-let mathOp;
+// let mathOp;
 
 function onReady() {
-  console.log('JQ Loaded');
-
   $('.js-btn-math').on('click', mathOperator);
   $('.js-btn-clear').on('click', mathClear);
+  $('.js-btn-equal').on('click', mathEqual);
+
+  render();
 }
 
 function mathOperator() {
   $(this).parent().find('button').removeClass('button-clicked');
   $(this).addClass('button-clicked');
-  mathOp = $(this).prop('name');
+  //   mathOp = $(this).prop('name');
+  //   console.log(mathOp);
 }
 
 function mathClear() {
   $(this).parent().find('button').removeClass('button-clicked');
   $('.js-input-first').val('');
   $('.js-input-second').val('');
-  mathOp = null;
+  //   mathOp = null;
+}
+
+function mathEqual() {
+  let mathOp = $('.button-clicked').prop('name');
+  let firstNumber = $('.js-input-first').val();
+  let secondNumber = $('.js-input-second').val();
+  //   if (mathOp === undefined) {
+  //     alert('Please select a math operator.');
+  //     return;
+  //   }
+  let inputs = [mathOp, firstNumber, secondNumber];
+  console.log(inputs);
+
+  $.ajax({
+    type: 'POST',
+    url: '/math',
+    data: { math: inputs },
+  })
+    .then(function (response) {
+      console.log(response);
+
+      // GET -> results
+      render();
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('Post failed');
+    });
+}
+
+function getResults() {
+  $.ajax({
+    type: 'GET',
+    url: '/results',
+  })
+    .then(function (response) {
+      render(response);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('Failed to retrieve results.');
+    });
+}
+
+function getHistory() {
+  $.ajax({
+    type: 'GET',
+    url: '/results/history',
+  })
+    .then(function (response) {
+      render(response);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('Failed to retrieve history.');
+    });
+}
+
+function renderResults(results) {
+  console.log(resultHistory);
+  const $results = $('.js-results');
+
+  $results.empty();
+  for (let i = 0; i < resultHistory.length; i++) {
+    const round = resultHistory[i];
+
+    $results.append(`<li>Round ${i + 1}</li>`);
+    for (let playerResults of round) {
+      $results.append(
+        `<li>${playerResults.name}, guessed ${playerResults.guess} <span>${playerResults.result}</span></li>`
+      );
+    }
+  }
+}
+
+function render(resultHistory) {
+  console.log(resultHistory);
+  const $results = $('.js-results');
+
+  $results.empty();
+  for (let i = 0; i < resultHistory.length; i++) {
+    const round = resultHistory[i];
+
+    $results.append(`<li>Round ${i + 1}</li>`);
+    for (let playerResults of round) {
+      $results.append(
+        `<li>${playerResults.name}, guessed ${playerResults.guess} <span>${playerResults.result}</span></li>`
+      );
+    }
+  }
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+let mathData;
+let mathResult;
+const mathHistory = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,13 +13,31 @@ app.use(bodyParser.json());
 app.use(express.static('server/public'));
 
 app.post('/math', (req, res) => {
-  // input data
-  // {
-  //  firstNumber: Number(),
-  //  secondNumber: Number(),
-  //  operation: '',
-  // }
-  // retrieve array from page containing both numbers and which input was hit
+  mathData = req.body.math;
+  //  array ['operator', num1, num2]
+  if (mathData[0] === 'add') {
+    mathResult = Number(mathData[1]) + Number(mathData[2]);
+    mathHistory.push(mathData[1] + ' + ' + mathData[2] + ' = ' + mathResult);
+  } else if (mathData[0] === 'subtract') {
+    mathResult = Number(mathData[1]) - Number(mathData[2]);
+    mathHistory.push(mathData[1] + ' - ' + mathData[2] + ' = ' + mathResult);
+  } else if (mathData[0] === 'multiply') {
+    mathResult = Number(mathData[1]) * Number(mathData[2]);
+    mathHistory.push(mathData[1] + ' * ' + mathData[2] + ' = ' + mathResult);
+  } else if (mathData[0] === 'divide') {
+    mathResult = Number(mathData[1]) / Number(mathData[2]);
+    mathHistory.push(mathData[1] + ' / ' + mathData[2] + ' = ' + mathResult);
+  }
+  console.log('Result', mathResult, 'History', mathHistory);
+  res.sendStatus(200);
+});
+
+app.get('/results', (req, res) => {
+  res.send(mathResult);
+});
+
+app.get('/results/history', (req, res) => {
+  res.send(mathHistory);
 });
 
 app.listen(PORT, () => {
